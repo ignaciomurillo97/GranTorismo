2b964332bc54090274675fdfcc5ea5db67224b2b
Ignacio Murillo Masis <imurillo97@estudiantec.cr>
2017-09-16 22:18:42 -0600

    Creando classe NeoConnection

---
+++ Gran Torismo API/Gran Torismo API/Models/NeoConnection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Neo4jClient;
+
+namespace NeoConnect
+{
+    public class NeoConnection
+    {
+        GraphClient graphClient = new GraphClient(new Uri("http://localhost:7474"), "neo4j", "caca");
+
+        private static NeoConnection instance;
+        private NeoConnection() { }
+        public static NeoConnection Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new NeoConnection();
+                }
+                return instance;
+            }
+        }
+
+        // Agrega un usuario 
+        public void agregarUsuario(int idUuario)
+        {
+            var usuario = new { id = idUuario };
+            graphClient.Cypher.Create("n:usuario{usuario}")
+                .WithParam("usuario", usuario)
+                .ExecuteWithoutResults();
+        }
+
+        // Agrega un producto
+        public void agregarProducto(int productId)
+        {
+            var producto = new { id = productId };
+            graphClient.Cypher.Create("n:producto{producto}")
+                .WithParam("producto", producto)
+                .ExecuteWithoutResults();
+        }
+    }
+}
