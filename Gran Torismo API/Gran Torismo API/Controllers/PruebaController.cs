4ac4be07101ce09137cc3fba3d776cd0e7e0fb4f
Ignacio Murillo Masis <imurillo97@estudiantec.cr>
2017-09-17 22:21:59 -0600

    Funcionalidad basica de Neo4j (agregar producto, usuario y relacion entre producto y usuario)

---
+++ Gran Torismo API/Gran Torismo API/Controllers/PruebaController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity.Core.Objects;
+using NeoConnect;
+using Gran_Torismo_API.Models;
+using Gran_Torismo_API.NeoHelper;
+
+namespace Gran_Torismo_API.Controllers
+{
+    public class PruebaController : ApiController
+    {
+
+        [Route("api/Usuario/Crear")]
+        public IHttpActionResult CrearUsuario(int idUsuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var neo = NeoConnection.Instance;
+            neo.agregarUsuario(idUsuario);
+            return Ok(idUsuario);
+        }
+
+        [Route("api/Producto/Crear")]
+        public IHttpActionResult CrearProducto(int idProducto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var neo = NeoConnection.Instance;
+            neo.agregarProducto(idProducto);
+            return Ok(idProducto);
+        }
+
+        [Route("api/Producto/Registrar/Vista")]
+        public IHttpActionResult VerProducto(int idUsuario, int idProducto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var neo = NeoConnection.Instance;
+            neo.verProducto(idUsuario, idProducto);
+            return Ok("orkas");
+        }
+
+        [Route("api/Usuario")]
+        public IHttpActionResult getAllUsers()
+        {
+            var neo = NeoConnection.Instance;
+            List<NeoUser> res = neo.getAllUsers();
+            return Ok(res);
+        }
+
+        [Route("api/Producto")]
+        public IHttpActionResult getAllProducts()
+        {
+            var neo = NeoConnection.Instance;
+            List<NeoProduct> res = neo.getAllProducts();
+            return Ok(res);
+        }
+    }
+}
--- Gran Torismo API/Gran Torismo API/Models/NeoConnection.cs
+++ Gran Torismo API/Gran Torismo API/Models/NeoConnection.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Neo4jClient;
+using Gran_Torismo_API.Models;
+using Gran_Torismo_API.NeoHelper;
 
 namespace NeoConnect
 {
     public class NeoConnection
     {
-        GraphClient graphClient = new GraphClient(new Uri("http://localhost:7474"), "neo4j", "caca");
+        GraphClient graphClient;
 
         private static NeoConnection instance;
-        private NeoConnection() { }
+        private NeoConnection()
+        {
+            graphClient = new GraphClient(new Uri("http://localhost:7474/db/data"), "neo4j", "caca");
+            graphClient.Connect();
+        }
+
         public static NeoConnection Instance
         {
             get
@@ -25,21 +32,52 @@ namespace NeoConnect
         }
 
         // Agrega un usuario 
-        public void agregarUsuario(int idUuario)
+        public void agregarUsuario(int idUsuario)
         {
-            var usuario = new { id = idUuario };
-            graphClient.Cypher.Create("n:usuario{usuario}")
+            var usuario = new { IdCard = idUsuario };
+            graphClient.Cypher.Create("(n:usuario{usuario})")
                 .WithParam("usuario", usuario)
                 .ExecuteWithoutResults();
         }
 
         // Agrega un producto
-        public void agregarProducto(int productId)
+        public void agregarProducto(int idProducto)
         {
-            var producto = new { id = productId };
-            graphClient.Cypher.Create("n:producto{producto}")
+            var producto = new { IdProduct = idProducto };
+            graphClient.Cypher.Create("(n:producto{producto})")
                 .WithParam("producto", producto)
                 .ExecuteWithoutResults();
+        }
+
+        // Agrega un producto 
+        public void verProducto(int idUsuario, int idProducto)
+        {
+            Dictionary<String, Object> parametrosRelacion = new Dictionary<string, object>();
+
+            graphClient.Cypher
+                .Match("(a:usuario), (b:producto)")
+                .Where((NeoUser a) => a.IdCard == idUsuario)
+                .AndWhere((NeoProduct b) => b.IdProduct == idProducto)
+                .Create("(a)-[:vio]->(b)")
+                .ExecuteWithoutResults();
+        }
+
+        public List<NeoUser> getAllUsers()
+        {
+            List<NeoUser> res = graphClient.Cypher
+                .Match("(user:usuario)")
+                .Return(user => user.As<NeoUser>())
+                .Results.ToList();
+            return res;
+        }
+
+        public List<NeoProduct> getAllProducts()
+        {
+            List<NeoProduct> res = graphClient.Cypher
+                .Match("(product:producto)")
+                .Return(product => product.As<NeoProduct>())
+                .Results.ToList();
+            return res;
         }
     }
 }
