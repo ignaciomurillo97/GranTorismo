6ae31202077424022267f5d032b792d2ccd6914f
cgranados16 <cgranados16@gmail.com>
2017-10-02 16:00:51 -0600

    prueba merge

--- SQLScripts/StoredProcedures.sql
+++ SQLScripts/StoredProcedures.sql
@@ -1,6 +1,7 @@
 USE GranTorismo
 GO
 	
+
 CREATE PROCEDURE [PR_CreateUser] (
 	@IdCard NUMERIC(20),
 	@Username VARCHAR(25),
@@ -23,7 +24,6 @@ CREATE PROCEDURE [PR_CreateUser] (
     END CATCH
 END
 GO
-
 
 CREATE PROCEDURE [PR_CreateClient] (
 	@IdCard NUMERIC(20),
@@ -49,6 +49,31 @@ CREATE PROCEDURE [PR_CreateClient] (
 	END
 END
 GO
+
+CREATE PROCEDURE [PR_CreateOwner] (
+	@IdCard NUMERIC(20),
+	@Username VARCHAR(25),
+    @Password VARCHAR(255),
+    @FirstName VARCHAR(50),
+    @MiddleName VARCHAR(50) = NULL,
+    @LastName VARCHAR(50),
+    @SecondLastName VARCHAR(50) = NULL,
+	@responseMessage NVARCHAR(250) OUTPUT
+) AS BEGIN
+	EXEC PR_CreateUser @IdCard, @Username, @Password, @FirstName, @MiddleName, @LastName, @SecondLastName, @responseMessage OUTPUT
+	IF @responseMessage = 'Success'
+	BEGIN
+		BEGIN TRY
+			INSERT INTO [Owner](IdCard) VALUES (@IdCard)
+		SET @responseMessage = 'Success'
+		END TRY
+		BEGIN CATCH
+			SET @responseMessage = ERROR_MESSAGE() 
+		END CATCH
+	END
+END
+GO
+
 
 CREATE PROCEDURE [PR_UserLogin](
     @Username NVARCHAR(254),
@@ -76,5 +101,167 @@ CREATE PROCEDURE [PR_ClientLogin](
 		EXEC PR_UserLogin @Username, @Password, @responseMessage OUTPUT, @IdCard OUTPUT
 	ELSE
 		SET @responseMessage = 'Invalid Login'
+END
+GO	
+
+CREATE PROCEDURE [PR_UsersLogin](
+    @Username NVARCHAR(254),
+    @Password NVARCHAR(50),
+    @responseMessage NVARCHAR(250)='' OUTPUT,
+	@IdCard NUMERIC(20) = NULL OUTPUT,
+	@Rol VARCHAR(20) = NULL OUTPUT
+)AS BEGIN
+	SET @Rol = CASE 
+		WHEN EXISTS(SELECT TOP 1 C.[IdCard] FROM [Client] C INNER JOIN [User] U ON U.[IdCard] = C.[IdCard] WHERE U.Username=@Username)
+			THEN 'Client' 
+		WHEN EXISTS(SELECT TOP 1 O.[IdCard] FROM [Owner] O INNER JOIN [User] U ON U.[IdCard] = O.[IdCard] WHERE U.Username=@Username)
+			THEN 'Owner'
+		WHEN EXISTS(SELECT TOP 1 A.[IdCard] FROM [Admin] A INNER JOIN [User] U ON U.[IdCard] = A.[IdCard] WHERE U.Username=@Username)
+			THEN 'Admin'
+		ELSE NULL END
+
+	IF (@Rol IS NULL)
+		SET @responseMessage = 'Invalid Login'
+	ELSE
+		EXEC PR_UserLogin @Username, @Password, @responseMessage OUTPUT, @IdCard OUTPUT
+END
+GO
+
+CREATE PROCEDURE [PR_GetUser](
+	@IdCard NUMERIC(20)
+)AS BEGIN
+	SELECT * FROM [ClientDetails] WHERE IdCard = @IdCard
+END
+GO
+
+CREATE PROCEDURE [PR_GetClient](
+	@IdCard NUMERIC(20)
+)AS BEGIN
+	SELECT CD.* FROM [ClientDetails] CD
+	INNER JOIN [Client] C ON CD.IdCard = C.IdCard
+	WHERE CD.IdCard = @IdCard
+END
+GO
+
+alter PROCEDURE [PR_GetUserByUsername](
+	@Username VARCHAR(25)
+)AS BEGIN
+	SELECT IdCard FROM [ClientDetails] WHERE Username = @Username
+END
+GO
+
+CREATE PROCEDURE [PR_EditCategory](
+	@Name VARCHAR(MAX),
+	@Id INT
+)AS BEGIN
+	UPDATE Category SET [Name] = @Name WHERE IdCategory = @Id
+END
+GO
+
+CREATE PROCEDURE [PR_CreateCategory] (
+	@Name VARCHAR(MAX)
+)AS BEGIN
+	INSERT INTO [Category] VALUES(@Name)
+END
+GO
+
+
+CREATE PROCEDURE [PR_DeleteCategory] (
+	@Id INT
+)AS BEGIN
+	DELETE [Category] WHERE IdCategory = @Id
+END
+GO
+
+CREATE PROCEDURE [PR_DeleteAdmin](
+	@id INT 
+) 
+AS BEGIN
+	DELETE [Admin] WHERE IdCard = @id
+	DELETE [User] WHERE IdCard = @id
+END
+GO
+
+CREATE PROCEDURE [PR_CreateAdmin] (
+	@IdCard NUMERIC(20),
+	@Username VARCHAR(25),
+    @Password VARCHAR(255),
+    @FirstName VARCHAR(50),
+    @MiddleName VARCHAR(50) = NULL,
+    @LastName VARCHAR(50),
+    @SecondLastName VARCHAR(50) = NULL,
+	@responseMessage NVARCHAR(250) OUTPUT
+) AS BEGIN
+	EXEC PR_CreateUser @IdCard, @Username, @Password, @FirstName, @MiddleName, @LastName, @SecondLastName, @responseMessage OUTPUT
+	IF @responseMessage = 'Success'
+	BEGIN
+		BEGIN TRY
+			INSERT INTO [Admin](IdCard) VALUES (@IdCard)
+		SET @responseMessage = 'Success'
+		END TRY
+		BEGIN CATCH
+			SET @responseMessage = ERROR_MESSAGE() 
+		END CATCH
+	END
+END
+GO
+
+CREATE PROCEDURE [PR_GetFollowing](
+	@IdCard NUMERIC(20)
+)AS BEGIN
+	SELECT U.* 
+	FROM [Follower] F
+	INNER JOIN [ClientDetails] U
+		ON F.IdFriend = U.IdCard
+	WHERE F.IdCard = @IdCard
+END
+GO
+
+CREATE PROCEDURE [PR_GetFollowers](
+	@IdCard NUMERIC(20)
+)AS BEGIN
+	SELECT U.* 
+	FROM [Follower] F
+	INNER JOIN [ClientDetails] U
+		ON F.IdFriend = @IdCard
+	WHERE F.IdCard = U.IdCard
+END
+GO
+
+
+CREATE PROCEDURE [PR_Follow](
+	@IdCard NUMERIC(20),
+	@IdFriend NUMERIC(20)
+)AS BEGIN
+	INSERT INTO [Follower] VALUES (@IdCard, @IdFriend)
+END
+GO
+
+CREATE PROCEDURE [PR_Unfollow](
+	@IdCard NUMERIC(20),
+	@IdFriend NUMERIC(20)
+)AS BEGIN
+	DELETE FROM [Follower]
+	WHERE IdCard = @IdCard AND IdFriend = @IdFriend
+END
+GO
+
+CREATE PROCEDURE [PR_GetDistricts]
+AS BEGIN
+SELECT 
+	IdDistrict AS [IdDistrict],
+	CONCAT(P.Name, ' - ', C.Name, ' - ', D.Name) AS [Name]
+FROM [Province] P
+	INNER JOIN [Canton] C ON C.IdProvince = P.IdProvince
+	INNER JOIN [District] D ON D.IdCanton = C.IdCanton
+END
+GO
+
+
+CREATE PROCEDURE [PR_CreateService]
+AS BEGIN
+	INSERT INTO [Service] ([State], [CreationDate])
+		VALUES (1, GETDATE())
+	SELECT @@IDENTITY
 END
 GO
